95b4a4abc24f61be35234e79e72d2c5afec2bc78
andrejvb <andre.jvb@gmail.com>
2023-02-09 20:36:18 -0300

    requisito 4 incompleto

--- src/components/WalletForm.jsx
+++ src/components/WalletForm.jsx
@@ -5,45 +5,98 @@ import fetchCurrency from '../services/api';
 import { saveCurrency } from '../redux/actions';
 
 class WalletForm extends Component {
+  state = {
+    value: '',
+    description: '',
+    currency: 'USD',
+    method: 'Dinheiro',
+    tag: 'Alimentação',
+  };
+
   async componentDidMount() {
     const { dispatch } = this.props;
     const coins = Object.keys(await fetchCurrency());
     dispatch(saveCurrency(coins));
   }
 
-  render() {
-    const { currency } = this.props;
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
 
-    const method = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
+  submitExpenses = () => {
+    const { dispatch } = this.props;
+    dispatch(...this.state);
+    this.setState({
+      value: '',
+      description: '',
+    });
+  };
+
+  render() {
+    const { currencies } = this.props;
+    const { value, description, currency, method, tag } = this.state;
+
+    const methods = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
     const categori = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
 
     return (
       <form>
         <label htmlFor="value">
           Valor:
-          <input type="text" id="value" data-testid="value-input" />
+          <input
+            type="text"
+            id="value"
+            name="value"
+            data-testid="value-input"
+            value={ value }
+            onChange={ this.handleChange }
+          />
         </label>
         <label htmlFor="description">
           Descrição:
-          <input type="text" id="description" data-testid="description-input" />
+          <input
+            type="text"
+            id="description"
+            name="description"
+            data-testid="description-input"
+            value={ description }
+            onChange={ this.handleChange }
+          />
         </label>
-        <select data-testid="currency-input">
+        <select
+          name="currency"
+          value={ currency }
+          data-testid="currency-input"
+          onChange={ this.handleChange }
+        >
           Moeda:
-          { currency.map((coin) => (
+          { currencies.map((coin) => (
             <option key={ coin } value={ coin }>
               { coin }
             </option>
           ))}
         </select>
-        <select data-testid="method-input">
+        <select
+          name="method"
+          value={ method }
+          data-testid="method-input"
+          onChange={ this.handleChange }
+        >
           Metodo:
-          { method.map((metodo) => (
+          { methods.map((metodo) => (
             <option key={ metodo } value={ metodo }>
               { metodo }
             </option>
           ))}
         </select>
-        <select data-testid="tag-input">
+        <select
+          id="tag"
+          name="tag"
+          value={ tag }
+          data-testid="tag-input"
+          onChange={ this.handleChange }
+        >
           Categoria:
           {categori.map((categoria) => (
             <option key={ categoria } value={ categoria }>
@@ -51,18 +104,19 @@ class WalletForm extends Component {
             </option>
           ))}
         </select>
+        <button type="button" onClick={ this.submitExpenses }>Adicionar despesa</button>
       </form>
     );
   }
 }
 
 WalletForm.propTypes = {
-  currency: PropTypes.arrayOf(PropTypes.string).isRequired,
+  currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
   dispatch: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  currency: state.wallet.currencies,
+  currencies: state.wallet.currencies,
 });
 
 export default connect(mapStateToProps)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,6 +2,8 @@ export const USER_LOGIN = 'USER_LOGIN';
 
 export const CURRENCY = 'CURRENCY';
 
+export const EXPENSES = 'EXPENSES';
+
 export const userLogin = (email) => ({
   type: USER_LOGIN,
   payload: email,
@@ -10,4 +12,9 @@ export const userLogin = (email) => ({
 export const saveCurrency = (coins) => ({
   type: CURRENCY,
   payload: coins.filter((coin) => coin !== 'USDT'),
+});
+
+export const saveExpenses = (despesas) => ({
+  type: EXPENSES,
+  expenses: despesas,
 });
